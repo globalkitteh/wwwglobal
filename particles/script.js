2de2d94c5956eba25978d5b1de9899a21111801f
Justin <peterjust.d@gmail.com>
2023-02-03 15:12:07 -0500

    Purdue theme

--- particles/script.js
+++ particles/script.js
@@ -1,31 +1,22 @@
 let fft
 
-global=0 // set design to my theming
+global = 1 // set design to my theming
 
 let Particle = function (position) {
   this.position = position
-  this.speed = createVector(0, random(0,100)/10)
-  if(global ==1){
-    this.color = [0,0,random(100,255)]
-  } else {
-    this.color = [random(197, 218),random(175, 195), random(135,155)]
-  }
+  this.speed = createVector(0, random(0, 100) / 10)
+  this.color = [random(197, 218), random(175, 195), random(135, 155)]
 
   this.draw = function () {
     circle(this.position.x, this.position.y, this.diameter)
     fill(this.color)
   }
   this.update = function (energy) {
-    this.position.y += (this.speed.y+0.0001) * (energy+0.01) * 10
+    this.position.y += (this.speed.y + 0.0001) * (energy + 0.01) * 10
     if (this.position.y > height) {
       this.position.y = 0
     }
-    this.diameter = (this.position.y * (random(5, 7) + energy*200))/1000 + 2
-
-    if(global ==1){
-      this.color = [this.color[1]+100,this.color[1],this.color[2]]
-    }
-
+    this.diameter = (this.position.y * (random(5, 7) + energy * 200)) / 1000 + 2
   }
 }
 
